ef3841163ad17a6db9fbbc007c09df6c4f94ea4e
antonio <ant.svirido@gmail.com>
2023-06-14 00:12:50 +0300

    add: unirx + hand simple animator logic

---
+++ Assets/Source/Scripts/Core/Input/HandAnimatorComponent.cs
@@ -0,0 +1,44 @@
+﻿using Source.Scripts.Core.Interfaces;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UniRx;
+
+namespace Source.Scripts.Core
+{
+    public class HandAnimatorComponent : BaseComponent
+    {
+        private Animator animator;
+        [SerializeField] private InputActionProperty triggerAction;
+        [SerializeField] private InputActionProperty gripAction;
+
+        private const string Trigger = "Trigger";
+        private const string Grip = "Grip";
+        
+        private void Awake()
+        {
+            InitComponentInGameObject(out animator);
+        }
+
+        private void OnEnable()
+        {
+            triggerAction.action.performed += HandleTrigger;
+            gripAction.action.performed += HandleGrip;
+        }
+
+        private void OnDisable()
+        {
+            triggerAction.action.performed -= HandleTrigger;
+            gripAction.action.performed -= HandleGrip;
+        }
+
+        private void HandleTrigger(InputAction.CallbackContext callbackContext)
+        {
+            animator.SetFloat(Trigger, triggerAction.action.ReadValue<float>());
+        }
+
+        private void HandleGrip(InputAction.CallbackContext callbackContext)
+        {
+            animator.SetFloat(Grip, gripAction.action.ReadValue<float>());
+        }
+    }
+}
