ff668c184f5644ccb6662ed3db1a79d0d0f34960
antonio <ant.svirido@gmail.com>
2023-06-15 21:41:42 +0300

    add: simple bullet (demo), simple pistol with grab and fire interact, add simple grab and interact demo objects

---
+++ Assets/Source/Scripts/Core/Interactable/SimpleWeaponsComponent/Weapon.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+namespace Source.Scripts.Core.Interactable
+{
+    public class Weapon : MonoBehaviour, IInteractableActivateListener
+    {
+        //TODO need create settings for different weapons
+        
+        [SerializeField] private GameObject bulletDemo;
+        [SerializeField] private Transform spawnPoint;
+        [SerializeField] private float speed;
+        
+        public void Interact(ActivateEventArgs args)
+        {
+            GameObject bullet = GetBullet();
+            bullet.transform.position = spawnPoint.position;
+            bullet.GetComponent<Rigidbody>().velocity = spawnPoint.forward * speed;
+        }
+
+        public GameObject GetBullet()
+        {
+            return Instantiate(bulletDemo); // todo change logic for bullets on object pool or smth another
+        }
+    }
+}
