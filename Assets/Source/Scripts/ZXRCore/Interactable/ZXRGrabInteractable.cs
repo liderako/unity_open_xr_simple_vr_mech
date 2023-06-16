4fb4d5f8b44a40c46d0bf8d8bb6c95817ff95609
antonio <ant.svirido@gmail.com>
2023-06-16 14:29:23 +0300

    add: installer for xr interaction manager, create zxrGrabInteractable and two attach grab (with installer interaction manager)

---
+++ Assets/Source/Scripts/ZXRCore/Interactable/ZXRGrabInteractable.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using Zenject;
+
+namespace ZXRCore.Interactable
+{
+    public class ZXRGrabInteractable : XRGrabInteractable
+    { 
+        [Inject]
+        private void InitializeInteractionManager(XRInteractionManager xrInteractionManager)
+        {
+            this.interactionManager = interactionManager;
+        }
+    }
+}
