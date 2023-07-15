51387180ea2817842b7dce73d26169bd6acf72eb
liderako <ant.svirido@gmail.com>
2023-07-15 23:58:32 +0300

    add: audio + trail for bullet

--- Assets/Source/Scripts/Entities/Interactable/SimpleWeaponsComponent/Weapon.cs
+++ Assets/Source/Scripts/Entities/Interactable/SimpleWeaponsComponent/Weapon.cs
@@ -1,21 +1,32 @@
-﻿using UnityEngine;
+﻿using System;
+using Source.Scripts.Core.Interfaces;
+using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
 namespace Source.Scripts.Core.Interactable
 {
-    public class Weapon : MonoBehaviour, IInteractableActivateListener
+    public class Weapon : BaseComponent, IInteractableActivateListener
     {
         //TODO need create settings for different weapons
         
         [SerializeField] private GameObject bulletDemo;
         [SerializeField] private Transform spawnPoint;
         [SerializeField] private float speed;
+        [SerializeField] private AudioClip clip;
         
+        private AudioSource source;
+
+        private void Start()
+        {
+            InitComponentInGameObject(out source);
+        }
+
         public void Interact(ActivateEventArgs args)
         {
             GameObject bullet = GetBullet();
             bullet.transform.position = spawnPoint.position;
             bullet.GetComponent<Rigidbody>().velocity = spawnPoint.forward * speed;
+            source.PlayOneShot(clip);
         }
 
         public GameObject GetBullet()
